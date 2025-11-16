9d6a95bd8e83af820204a1b1d48f32a07d3aee10
SauWagh <109113666+SauWagh@users.noreply.github.com>
2025-11-16 20:04:45 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -157,10 +157,13 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/' 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
-EMAIL_HOST = 'smtp.gmail.com'                                   
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('sw750978@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('wruratuaaomkiypj')
+
+# Read from environment variables
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
