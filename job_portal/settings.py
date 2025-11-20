72cfdf4e3c7132940d6aa89c7345737825c8f335
SauWagh <109113666+SauWagh@users.noreply.github.com>
2025-11-20 15:21:03 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -46,7 +46,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['https://job-portal-c6qo.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "job-portal-c6qo.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
