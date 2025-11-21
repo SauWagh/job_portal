fa5b979c9ecad79dbada8b3cf26787295fb68745
SauWagh <109113666+SauWagh@users.noreply.github.com>
2025-11-21 12:56:27 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -167,6 +167,21 @@ else:
         }
     }
 
+import logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
